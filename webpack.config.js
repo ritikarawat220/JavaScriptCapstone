760bd9677f1eeccd9d61b5e8f12dd92a503a0824
Ritika Rawat <Ritikarawat220@gmail.com>
2023-03-21 10:37:25 +0530

    Add styling for footer

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
  module.exports = {
     mode: "development",
-   entry: './src/index.js',
+   entry: "./src/index.js",
    output: {
-     filename: 'bundle.js',
-     path: path.resolve(__dirname, 'dist'),
+     filename: "bundle.js",
+     path: path.resolve(__dirname, "dist"),
      clean: true,
    },
    plugins: [
     new HtmlWebpackPlugin({
-      title: 'group-capstone',
+      title: "group-capstone",
       template: "./src/index.html",
     }),
   ],
