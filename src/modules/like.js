509d2fbd552658976bc27f0fa944e6f141fe482a
Tracy-miranja <barazatracy44@gmail.com>
2023-03-22 12:56:13 +0300

    add logo

---
+++ src/modules/like.js
@@ -0,0 +1,7 @@
+const fetchLikes = () => {
+    fetch("https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/")
+    .then(response => response.jason())
+    .then(object =>{
+
+    });
+}
