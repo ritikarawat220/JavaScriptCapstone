5ee779279b7ef5f1560e808dd5ecde1c7f3f8144
Ritika Rawat <Ritikarawat220@gmail.com>
2023-03-22 18:54:46 +0530

    Add comments to display in window

--- src/modules/mealInfoModel.js
+++ src/modules/mealInfoModel.js
@@ -27,8 +27,8 @@ function mealInfoModal(meal) {
             <div class="comment-btn">
               <h3 class="modal-title">Comments</h3>             
             </div>
-            <div class="show-comments">Hello</div>
-            <div class="show-comments">Very Delicious</div>
+            <div class="show-comments"></div>
+            <div class="show-comments"></div>
             <div class="add-comment">            
               <form action="#" class="form">
                 <h3>Add a comment</h3>
@@ -40,5 +40,48 @@ function mealInfoModal(meal) {
     `;
   modalDetailsContent.innerHTML = html;
   modalDetailsContent.parentElement.classList.add("showModal");
+
+  const commentForm = document.querySelector(".form");
+const commentSection = document.querySelector(".show-comments");
+const nameInput = document.querySelector("#name");
+const commentInput = document.querySelector("#comment-text");
+
+commentForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+
+  const comment = {
+    name: nameInput.value,
+    comment: commentInput.value,
+    
+  };
+
+  let comments = JSON.parse(localStorage.getItem("comments")) || [];
+
+  // Add the new comment to the array and update local storage
+  comments.push(comment);
+  localStorage.setItem("comments", JSON.stringify(comments));
+
+  // Clear the form inputs
+  nameInput.value = "";
+  commentInput.value = "";
+
+  // Display the new comment in the comment section
+  const html = `<div class="comment">
+                  <p class="comment-info">${comment.name} : ${comment.comment}</p>
+                </div>`;
+  commentSection.insertAdjacentHTML("beforeend", html);
+});
+
+// Display any existing comments on page load
+let comments = JSON.parse(localStorage.getItem("comments")) || [];
+comments.forEach((comment) => {
+  const html = `<div class="comment">
+                  <p class="comment-info">${comment.name} : ${comment.comment}</p>
+                </div>`;
+  commentSection.insertAdjacentHTML("beforeend", html);
+});
+
+
 }
 export default mealInfoModal;
