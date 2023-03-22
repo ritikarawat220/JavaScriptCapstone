d3a4f89ac16a14e6d5170a7d49273cb58e94cc77
Tracy-miranja <barazatracy44@gmail.com>
2023-03-22 14:54:46 +0300

    Add interactivity to my like image

--- src/modules/like.js
+++ src/modules/like.js
@@ -1,7 +1,22 @@
-const fetchLikes = async () => {
+
     fetch("https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/WxdOldIVe5Cky63nkl0B/likes")
     .then(response => response.jason())
     .then(object =>{
-
+        fetchLikes(object);
     });
-}
+
+    function fetchLikes(object){
+      const likesDiv= document.querySelector(".like-div");
+      const boredomActivity=document.querySelector(".like");
+      boredomActivity.innerHTML= object.activity;
+      const likebtn= document.querySelector(".like-btn");
+      const likeCount= document.querySelector(".like-count");
+
+      let likes = 0;
+      function incrementLikes(){
+        likes++;
+        likeCount.innerHTML= likes;
+      }
+      
+
+    }
