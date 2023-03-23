bc280731e57f39bf1b284fbb16bdf928492fe707
Tracy-miranja <barazatracy44@gmail.com>
2023-03-23 09:54:22 +0300

    Made changes to Api.js fot like btn

--- src/modules/like.js
+++ src/modules/like.js
@@ -1,32 +1,32 @@
 
-// fetch("https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/WxdOldIVe5Cky63nkl0B/likes/")
-//     .then(response => response.json())
-//     .then(object =>{
-//         fetchLikes(object);
-//     });
+fetch("https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/WxdOldIVe5Cky63nkl0B/likes/")
+    .then(response => response.json())
+    .then(object =>{
+        fetchLikes(object);
+    });
 
-//     function fetchLikes(object){
-//       const likesDiv= document.querySelector(".like-div");
-//       const boredomActivity=document.createElement("p");
-//       boredomActivity.innerHTML= object.activity;
-//       const likebtn= document.querySelector(".like-btn");
-//       const likeCount= document.querySelector(".like-count");
+    function fetchLikes(object){
+      const likesDiv= document.querySelector(".like-div");
+      const boredomActivity=document.createElement("p");
+      boredomActivity.innerHTML= object.activity;
+      const likebtn= document.querySelector(".like-btn");
+      const likeCount= document.querySelector(".like-count");
 
-//       let likes = 0;
-//       function incrementLikes(){
-//         likes++;
-//         if(likes===1){
-//         likeCount.innerHTML= `${likes} Like`;
-//         }else{
-//             likeCount.innerHTML= `${likes} Likes`;
-//         }
-//         likebtn.classList.add("liked");
-//       }
+      let likes = 0;
+      function incrementLikes(){
+        likes++;
+        if(likes===1){
+        likeCount.innerHTML= `${likes} Like`;
+        }else{
+            likeCount.innerHTML= `${likes} Likes`;
+        }
+        likebtn.classList.add("liked");
+      }
       
-//       likebtn.addEventListener("click", incrementLikes);
-//       boredomActivity.appendChild(likebtn);
-//       boredomActivity.appendChild(likeCount);
-//       likesDiv.appendChild(boredomActivity);
+      likebtn.addEventListener("click", incrementLikes);
+      boredomActivity.appendChild(likebtn);
+      boredomActivity.appendChild(likeCount);
+      likesDiv.appendChild(boredomActivity);
 
-//     }
-//     export default fetchLikes;
+    }
+    export default fetchLikes;
