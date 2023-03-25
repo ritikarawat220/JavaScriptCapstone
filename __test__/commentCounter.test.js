b3e058969a43fa2b6e71762b4d3fd8896798133a
Ritika Rawat <Ritikarawat220@gmail.com>
2023-03-25 08:10:59 +0530

    All test passed

--- __test__/commentCounter.test.js
+++ __test__/commentCounter.test.js
@@ -5,17 +5,22 @@
 
 import commentCount from "../src/modules/commentCounter.js";
 /* eslint-disable */
+const { JSDOM } = require('jsdom');
+const dom = new JSDOM(`<!DOCTYPE html><body></body></html>`);
+global.window = dom.window;
+global.document = window.document;
+
 describe("Counters", () => {
   it("Comments counters", () => {
     document.body.innerHTML = `<div class="left">
-  <p class="eachScore">
-    2022-09-01
-    <span>Ritika:</span>
-  </p>
-  <span>
-    <p class="numberSc">Great</p>
-  </span>
-</div>`;
+      <p class="eachScore">
+        2022-09-01
+        <span>Ritika:</span>
+      </p>
+      <span>
+        <p class="numberSc">Great</p>
+      </span>
+    </div>`;
     const count = document.querySelectorAll(".left");
     expect(commentCount(count)).toBe(1);
   });
